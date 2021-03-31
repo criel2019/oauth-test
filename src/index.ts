0fae2530bc00a63ff09ad18eccc29ed941722fe7
criel2019 <pjhk579700@gmail.com>
2021-03-31 21:40:01 +0900

    callbackURL has been changed

--- src/index.ts
+++ src/index.ts
@@ -61,7 +61,7 @@ passport.deserializeUser((id: string, done: any) => {
 passport.use(new GoogleStrategy({
   clientID: `${process.env.GOOGLE_CLIENT_ID}`,
   clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
-  callbackURL: "http://localhost:3000/auth/google/callback"
+  callbackURL: "/auth/google/callback"
 },
   function (_: any, __: any, profile: any, cb: any) {
 
