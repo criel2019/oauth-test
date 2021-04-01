90ed3d839a32307274db97cfe56e16758fef2244
criel2019 <pjhk579700@gmail.com>
2021-04-01 18:06:40 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,7 @@ passport.deserializeUser((id: string, done: any) => {
 passport.use(new GoogleStrategy({
   clientID: `${process.env.GOOGLE_CLIENT_ID}`,
   clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
-  callbackURL: "/auth/google/callback"
+  callbackURL: "https://criel-test.herokuapp.com/auth/google/callback"
 },
   function (_: any, __: any, profile: any, cb: any) {
 
@@ -149,9 +149,9 @@ passport.use(new GitHubStrategy({
 
 
 
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
+app.get('https://criel-test.herokuapp.com/auth/google', passport.authenticate('google', { scope: ['profile'] }));
 
-app.get('/auth/google/callback',
+app.get('https://criel-test.herokuapp.com/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/login', session: true }),
   function (req, res) {
     res.redirect('/');
