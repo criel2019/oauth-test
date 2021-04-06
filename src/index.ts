70d50a95eaf44d6f7104e20ec90d589d2d4fa70d
criel2019 <pjhk579700@gmail.com>
2021-04-06 21:35:00 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,12 @@ import dotenv from "dotenv";
 import config from './config/config'
 import userRoutes from './routes/user'
 import session from 'express-session'
-import passport from 'passport'
-import Strategy  from './controller/user'
+import passport, { serializeUser, Strategy } from 'passport';
+import User from './models/user';
+import { IMongoDBUser } from "src/interfaces/user";
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const NaverStrategy = require('passport-naver').Strategy;
+const KakaoStrategy = require('passport-kakao').Strategy;
 
 dotenv.config();
 
@@ -43,7 +47,120 @@ app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-Strategy;
+
+
+passport.serializeUser((user: IMongoDBUser, done: any) => {
+  return done(null, user._id);
+});
+
+passport.deserializeUser((id: string, done: any) => {
+
+  User.findById(id, (err: Error, doc: IMongoDBUser) => {
+    // Whatever we return goes to the client and binds to the req.user property
+    return done(null, doc);
+  })
+})
+
+passport.use(new GoogleStrategy({
+  clientID: `${process.env.GOOGLE_CLIENT_ID}`,
+  clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
+  callbackURL: "/user/auth/google/callback"
+},
+
+ function (_: any, __: any, profile: any, cb: any) {
+
+    User.findOne({ googleId: profile.id }, async (err: Error, doc: IMongoDBUser) => {
+
+      if (err) {
+        return cb(err, null);
+      }
+
+      if (!doc) {
+        const newUser = new User({
+          googleId: profile.id,
+          username: profile.name.givenName
+        });
+
+        await newUser.save();
+        cb(null, newUser);
+
+      }
+      cb(null, doc);
+
+
+
+    })
+
+  }));
+
+
+
+
+
+
+
+ passport.use(new NaverStrategy({
+  clientID: `xeiJ4Etz63ZzWGd_3ODf`,
+  clientSecret: `i25diPbhrQ`,
+  callbackURL: "/user/auth/naver/callback"
+},
+  function (_: any, __: any, profile: any, cb: any) {
+
+    User.findOne({ naverId: profile.id }, async (err: Error, doc: IMongoDBUser) => {
+
+      if (err) {
+        return cb(err, null);
+      }
+
+      if (!doc) {
+        const newUser = new User({
+          naverId: profile.id,
+          username: profile.username
+        });
+
+        await newUser.save();
+        cb(null, newUser);
+      }
+      cb(null, doc);
+    })
+
+  }
+));
+
+
+
+
+
+passport.use(new KakaoStrategy({
+  clientID: `${process.env.Kakao_CLIENT_ID}`,
+  clientSecret: `${process.env.Kakao_CLIENT_SECRET}`,
+  callbackURL: "/user/auth/kakao/callback"
+},
+  function (_: any, __: any, profile: any, cb: any) {
+
+    User.findOne({ kakaoId: profile.id }, async (err: Error, doc: IMongoDBUser) => {
+
+      if (err) {
+        return cb(err, null);
+      }
+
+      if (!doc) {
+        const newUser = new User({
+          kakaoId: profile.id,
+          username: profile.username
+        });
+
+        await newUser.save();
+        cb(null, newUser);
+      }
+      cb(null, doc);
+    })
+
+  }
+));
+
+
+
 
 app.use('/user', userRoutes);
 
