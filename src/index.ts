07ee31480ecba1c9f0a0d35e90afcec39b5062bf
criel2019 <pjhk579700@gmail.com>
2021-04-07 13:51:32 +0900

    .

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,15 @@
 import express from 'express'
-import cors from "cors"
 import mongoose, { Error } from 'mongoose';
 import dotenv from "dotenv";
 import config from './config/config'
 import userRoutes from './routes/user'
-import session from 'express-session'
-import passport from 'passport';
-import User from './models/user';
-import { IMongoDBUser } from "./interfaces/user";
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const NaverStrategy = require('passport-naver').Strategy;
-const KakaoStrategy = require('passport-kakao').Strategy;
+
+
 
 dotenv.config();
 
 const app = express()
-
+app.set("trust proxy", 1);
 mongoose.connect(`${process.env.START_MONGODB}${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}${process.env.END_MONGODB}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -23,166 +17,6 @@ mongoose.connect(`${process.env.START_MONGODB}${process.env.MONGODB_USERNAME}:${
   console.log("Connected to mongoose successfully")
 });
 
-
-// Middleware changed 
-app.use(express.json());
-app.use(cors({ origin: "https://criel-front.netlify.app", credentials: true }))
-
-app.set("trust proxy", 1);
-
-app.use(
-  session({
-    secret: "secretcode",
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-      sameSite: "none",
-      secure: true,
-      maxAge: 1000 * 60 * 60 * 24 * 7 // One Week
-    }
-  }))
-app.use(express.urlencoded({ extended: false }));
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-
-
-passport.serializeUser((user: IMongoDBUser, done: any) => {
-  console.log('serializeUser', user)  
-  return done(null, user._id);
-});
-
-passport.deserializeUser((id: string, done: any) => {
-  console.log('deserializeUser', id)  
-  User.findById(id, (err: Error, doc: IMongoDBUser) => {
-    // Whatever we return goes to the client and binds to the req.user property
-    return done(null, doc);
-  })
-})
-
-
-passport.use(new GoogleStrategy({
-  clientID: `${process.env.GOOGLE_CLIENT_ID}`,
-  clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
-  callbackURL: "/auth/google/callback"
-},
-
- function (_: any, __: any, profile: any, cb: any) {
-
-    User.findOne({ googleId: profile.id }, async (err: Error, doc: IMongoDBUser) => {
-
-      if (err) {
-        return cb(err, null);
-      }
-
-      if (!doc) {
-        const newUser = new User({
-          googleId: profile.id,
-          username: profile.name.givenName
-        });
-
-        await newUser.save();
-        cb(null, newUser);
-
-      }
-      cb(null, doc);
-
-
-
-    })
-
-  }));
-
-
-
-
-
-
-
- passport.use(new NaverStrategy({
-  clientID: `xeiJ4Etz63ZzWGd_3ODf`,
-  clientSecret: `i25diPbhrQ`,
-  callbackURL: "/auth/naver/callback"
-},
-  function (_: any, __: any, profile: any, cb: any) {
-
-    User.findOne({ naverId: profile.id }, async (err: Error, doc: IMongoDBUser) => {
-
-      if (err) {
-        return cb(err, null);
-      }
-
-      if (!doc) {
-        const newUser = new User({
-          naverId: profile.id,
-          username: profile.username
-        });
-
-        await newUser.save();
-        cb(null, newUser);
-      }
-      cb(null, doc);
-    })
-
-  }
-));
-
-
-
-
-
-passport.use(new KakaoStrategy({
-  clientID: `${process.env.Kakao_CLIENT_ID}`,
-  clientSecret: `${process.env.Kakao_CLIENT_SECRET}`,
-  callbackURL: "/auth/kakao/callback"
-},
-  function (_: any, __: any, profile: any, cb: any) {
-
-    User.findOne({ kakaoId: profile.id }, async (err: Error, doc: IMongoDBUser) => {
-
-      if (err) {
-        return cb(err, null);
-      }
-
-      if (!doc) {
-        const newUser = new User({
-          kakaoId: profile.id,
-          username: profile.username
-        });
-
-        await newUser.save();
-        cb(null, newUser);
-      }
-      cb(null, doc);
-    })
-
-  }
-));
-
-
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
-app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login', session: true }),
-  function (req, res) {
-    res.redirect('https://criel-front.netlify.app');
-  });
-  app.get('/auth/kakao', passport.authenticate('kakao'));
-
-  app.get('/auth/kakao/callback',
-    passport.authenticate('kakao', { failureRedirect: '/login', session: true }),
-    function (req, res) {
-      res.redirect('https://criel-front.netlify.app');
-    });
-  
- 
-app.get('/auth/naver', passport.authenticate('naver'));
-
-app.get('/auth/naver/callback',
-  passport.authenticate('naver', { failureRedirect: '/login', session: true }),
-  function (req, res) {
-    res.redirect('https://criel-front.netlify.app');
-  });
 
 
 
