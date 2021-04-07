b1ab69e6ea1c78128cb8fe65f1d6a6c1136bbe38
criel2019 <pjhk579700@gmail.com>
2021-04-07 10:39:02 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -206,7 +206,7 @@ app.get("/auth/logout", (req, res) => {
      req.logout();
      res.send("done");
    }
-})
+)
 
 const port = process.env.PORT || 8081
 app.listen(port,()=>console.log("start"+port))
